93942b2a76009c864f9df16d2f1868711e03f68e
Marcelo Waki <wakimarcelo@outlook.com>
2024-01-06 20:58:13 -0300

    Primeiro projeto C#/.NET

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+if (daysUntilExpiration == 0)
+{
+  Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+  discountPercentage = 20;
+  Console.WriteLine("Your subscription expires within a day!");
+  Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
+else if (daysUntilExpiration <= 5)
+{
+  discountPercentage = 10;
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+  Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
+else if (daysUntilExpiration <= 10)
+{
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
